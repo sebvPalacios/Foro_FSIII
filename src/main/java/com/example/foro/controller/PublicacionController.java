37eb3ffccf88b761f3588703bb9d44bf6ce40eab
sebvPalacios <se.palaciosm@duocuc.cl>
2025-03-24 13:49:54 -0300

    Eliminacion de algunas importaciones que no se utilizaban

--- src/main/java/com/example/foro/controller/PublicacionController.java
+++ src/main/java/com/example/foro/controller/PublicacionController.java
@@ -2,7 +2,6 @@ package com.example.foro.controller;
 
 import java.util.List;
 import java.util.Optional;
-import com.example.foro.service.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ import com.example.foro.service.PublicacionService;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
